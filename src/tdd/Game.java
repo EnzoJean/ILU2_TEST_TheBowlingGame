d0c9bd7c8407968d9cabe2e9d0584069285b7f81
Enzo Jean <jnn4376a@ensfsi.onmicrosoft.com>
2024-04-11 16:49:44 +0200

    Niveau1 fontionne

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -1,5 +1,17 @@
 package tdd;
 
 public class Game {
-
+	private int score;
+	
+	public Game() {
+		score = 0;
+	}
+	
+	public void roll(int nbQuilles) {
+		score += nbQuilles;
+	}
+	
+	public int score() {
+		return score;
+	}
 }
--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -1,5 +1,41 @@
 package tdd;
 
-public class GameTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+
+public class GameTest {
+	private Game game = new Game();
+	
+	@Test
+	public void test1Niveau1() {
+		game.roll(0);
+		assertEquals(game.score(), 0);
+	}
+	
+	@Test
+	public void test2Niveau1() {
+		for (int i =0; i<20; ++i) {
+			game.roll(0);
+		}
+		assertEquals(game.score(), 0);
+	}
+	
+	@Test
+	public void test3Niveau1() {
+		for (int i =0; i<20; ++i) {
+			game.roll(1);
+		}
+		assertEquals(game.score(), 20);
+	}
+	
+	@Test
+	public void test4Niveau1() {
+		for (int i =0; i<10; ++i) {
+			game.roll(1);
+			game.roll(2);
+		}
+		assertEquals(game.score(), 30);
+	}
 }
